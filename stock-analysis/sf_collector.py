ea0169dc63cbc32e7804151f99fc1e0cb7778761
hhanccheng <hccao12o@gmail.com>
2022-05-07 16:03:59 -0400

    5722403

--- stock-analysis/sf_collector.py
+++ stock-analysis/sf_collector.py
@@ -9,6 +9,7 @@ import time
 def stock_collect(stockid,start,end):
     try:
         site = "https://query1.finance.yahoo.com/v8/finance/chart/%s?period1=%d&period2=%d&interval=1d&events=history&=hP2rOschxO0" % (stockid,start,end)
+        print (site)
         response = requests.get(site)
         data = json.loads(response.text)
         df = pd.DataFrame(data['chart']['result'][0]['indicators']['quote'][0], index=pd.to_datetime(np.array(data['chart']['result'][0]['timestamp'])*1000*1000*1000))
@@ -17,10 +18,6 @@ def stock_collect(stockid,start,end):
         return df
     except:
         return None
-
-end = int(time.time())
-start = end - (3600*24*600)
-stock_collect('AMD',start,end)
 
 
 
